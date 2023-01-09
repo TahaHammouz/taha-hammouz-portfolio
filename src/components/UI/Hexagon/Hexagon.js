b97cc90d477dcaf04659e4c0a4428e3fd3eb70cd
TahaHammouz <t.r.hamooz@gmail.com>
2023-01-09 22:44:29 +0200

    Home page

---
+++ src/components/UI/Hexagon/Hexagon.js
@@ -0,0 +1,13 @@
+const Hexagon1 = () => {
+  return (
+    <img src={require("../../../assets/images/Vector1.png")} alt="filled Hexagon" />
+  );
+};
+
+const Hexagon0 = () => {
+  return (
+    <img src={require("../../../assets/images/Vector0.png")} alt="empty Hexagon" />
+  );
+};
+
+export { Hexagon1, Hexagon0 };
